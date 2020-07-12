4fe4f4b2df9023144cb7e99ad58882c752eeb7a4
David <Davidp809@gmail.com>
2020-07-12 14:54:55 -0400

    Creating PresetViewModels

---
+++ LulCaster.UI.WPF/ViewModels/PresetViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LulCaster.UI.WPF.ViewModels
+{
+  public class PresetViewModel
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string FilePath { get; set; }
+  }
+}
