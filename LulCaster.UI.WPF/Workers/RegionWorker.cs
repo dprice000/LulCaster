90b3788e8b8daffcbf4427995dd7eb5a67addeed
David <Davidp809@gmail.com>
2020-12-16 01:32:33 -0500

    Creating a pool of Region processors that can process a number of regions at a time.

---
+++ LulCaster.UI.WPF/Workers/RegionWorker.cs
@@ -0,0 +1,106 @@
+﻿using LulCaster.UI.WPF.Utility;
+using LulCaster.UI.WPF.ViewModels;
+using LulCaster.Utility.Service;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace LulCaster.UI.WPF.Workers
+{
+  public class RegionWorker : LulWorkerBase
+  {
+    private RegionViewModel _region;
+    private ScreenCapture _screenCapture;
+    private IOcrService _ocrService = new OcrService();
+
+    public RegionWorker(ScreenCapture screenCapture, RegionViewModel region)
+    {
+      _region = region;
+      _screenCapture = screenCapture;
+    }
+
+    protected override void DoWork()
+    {
+      if (_region.BoundingBox.Width < 1 || _region.BoundingBox.Height < 1)
+      {
+        Stop();
+        return;
+      }
+       
+      using (var croppedImage = CropBitmap(_screenCapture, _region.BoundingBox))
+      {
+        //TODO: Do we want to remove this? Or do we want to do it better?
+        //using (MemoryStream memory = new MemoryStream())
+        //{
+        //  using (FileStream fs = new FileStream(@"C:\Users\David\Documents\Overwatch\poo2.bmp", FileMode.Create, FileAccess.ReadWrite))
+        //  {
+        //    croppedImage.Save(memory, ImageFormat.Jpeg);
+        //    byte[] bytes = memory.ToArray();
+        //    fs.Write(bytes, 0, bytes.Length);
+        //  }
+        //}
+
+        var scrappedText = ScrapeImage(croppedImage);
+        ProcessTriggers(_region.Triggers, _screenCapture, scrappedText);
+      }
+
+      IsRunning = false;
+    }
+
+    private Bitmap CropBitmap(ScreenCapture screenCapture, Rectangle regionBoundingBox)
+    {
+      using (var image = Image.FromStream(screenCapture.ScreenMemoryStream))
+      {
+        var relativeBounds = AspectRatioConverter.ResolveScreenLocation(screenCapture.ScreenBounds, screenCapture.CanvasBounds, regionBoundingBox);
+
+        Bitmap croppedBitmap = new Bitmap(relativeBounds.Width, relativeBounds.Height);
+
+        using (Graphics graphics = Graphics.FromImage(croppedBitmap))
+        {
+          graphics.DrawImage(image, new Rectangle(0, 0, croppedBitmap.Width, croppedBitmap.Height), relativeBounds,
+                           GraphicsUnit.Pixel);
+        }
+
+        return croppedBitmap;
+      }
+    }
+
+    private void ProcessTriggers(IList<TriggerViewModel> triggers, ScreenCapture screenCapture, string scrappedText)
+    {
+      foreach (var trigger in triggers)
+      {
+        //TODO: This needs to eventually tap into a trigger factory.
+        if (scrappedText.Contains(trigger.ThresholdValue))
+        {
+          Console.WriteLine($"Seconds before trigger was processed: {DateTime.Now.Subtract(screenCapture.CreationTime).TotalSeconds}");
+        }
+      }
+    }
+
+    private string ScrapeImage(Bitmap image)
+    {
+      using (MemoryStream memoryStream = new MemoryStream())
+      {
+        image.Save(memoryStream, ImageFormat.Tiff);
+
+        return _ocrService.ProcessImage(memoryStream);
+      }
+    }
+
+    private BitmapImage ConvertStreamToBitmap(MemoryStream imageStream)
+    {
+      var imageBitmap = new BitmapImage();
+      imageBitmap.BeginInit();
+      imageBitmap.StreamSource = imageStream;
+      imageBitmap.CacheOption = BitmapCacheOption.OnLoad;
+      imageBitmap.EndInit();
+      imageBitmap.Freeze();
+
+      return imageBitmap;
+    }
+  }
+}
---
+++ LulCaster.UI.WPF/Workers/RegionWorkerPool.cs
@@ -0,0 +1,93 @@
+﻿using LulCaster.UI.WPF.ViewModels;
+using LulCaster.UI.WPF.Workers.EventArguments;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LulCaster.UI.WPF.Workers
+{
+  public class RegionWorkerPool : LulWorkerBase
+  {
+    public event EventHandler<ScreenCaptureProgressArgs> ProgressChanged;
+
+    public event EventHandler<TriggerViewModel> TriggerActivated;
+
+    private const int IDLE_HALT_TIMEOUT = 100;
+
+    private readonly ConcurrentQueue<ScreenCapture> _screenCaptureQueue = new ConcurrentQueue<ScreenCapture>();
+    private readonly List<RegionWorker> _regionWorkers = new List<RegionWorker>();
+
+    public bool IsFull
+    {
+      get
+      {
+        return _regionWorkers.Count <= MaxPoolSize;
+      }
+    }
+    
+    public int MaxPoolSize { get; }
+
+    public RegionWorkerPool(int maxPoolSize)
+    {
+      MaxPoolSize = maxPoolSize;
+    }
+
+    public void EnqueueScreenCapture(ScreenCapture screenCapture)
+    {
+      _screenCaptureQueue.Enqueue(screenCapture);
+    }
+
+    private void OnTriggerActivated(TriggerViewModel trigger)
+    {
+      TriggerActivated?.Invoke(this, trigger);
+    }
+
+    protected override void DoWork()
+    {
+      while (IsRunning)
+      {
+        if (_screenCaptureQueue.IsEmpty 
+            || IsFull
+            || !_screenCaptureQueue.TryDequeue(out ScreenCapture screenCapture))
+        {
+          Wait(IDLE_HALT_TIMEOUT);
+          continue;
+        }
+
+        using (screenCapture)
+        {
+          foreach (var region in screenCapture.RegionViewModels)
+          {
+            WaitForFreeWorker();
+            CreateWorker(screenCapture, region);
+          }
+        }
+      }
+    }
+
+    private void WaitForFreeWorker()
+    {
+      while (IsFull)
+      {
+        Wait(IDLE_HALT_TIMEOUT);
+        PruneFinishedWorkers();
+      }
+    }
+
+    private void PruneFinishedWorkers()
+    {
+      foreach (var worker in _regionWorkers.Where(x => !x.IsRunning))
+      {
+        _regionWorkers.Remove(worker);
+      }
+    }
+
+    private void CreateWorker(ScreenCapture screenCapture, RegionViewModel region)
+    {
+      var worker = new RegionWorker(screenCapture, region);
+      worker.Start();
+      _regionWorkers.Add(worker);
+    }
+  }
+}
