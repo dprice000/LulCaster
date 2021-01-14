fae701d9bd352fd1cbc1561d4add0eee79a84d32
David <Davidp809@gmail.com>
2021-01-13 23:16:55 -0500

    Created static class to handle mocking dialogs

---
+++ LulCaster.Test.UI.WPF/Utility/DialogMocker.cs
@@ -0,0 +1,31 @@
+﻿using LulCaster.UI.WPF.Dialogs;
+using LulCaster.UI.WPF.Dialogs.Models;
+using LulCaster.UI.WPF.Dialogs.Providers;
+using LulCaster.UI.WPF.Dialogs.Services;
+using LulCaster.UI.WPF.Dialogs.ViewModels;
+using LulCaster.UI.WPF.ViewModels;
+using NSubstitute;
+
+namespace LulCaster.Test.UI.WPF.Utility
+{
+  public static class DialogMocker
+  {
+    public static void InitializeOkCancelDialog(PresetViewModel dialogViewModel, DialogResults buttonResult)
+    {
+      var dialogResult = new NestedDialogResults<PresetViewModel>(dialogViewModel, buttonResult);
+      var dialogService = Substitute.For<INestedViewDialog<PresetViewModel>>();
+      dialogService.Show(Arg.Any<NestedViewModel<PresetViewModel>>()).Returns(dialogResult);
+
+      CrudDialogProvider.AddDialog<PresetViewModel>(dialogService);
+    }
+
+    public static void InitializeYesNoDialog(DialogResults dialogResult)
+    {
+      var result = new LulDialogResult() { DialogResult = dialogResult };
+      var dialogService = Substitute.For<IDialogService<MessageBoxDialog, LulDialogResult>>();
+      dialogService.Show(Arg.Any<string>(), Arg.Any<string>(), DialogButtons.YesNo).Returns(result);
+
+      var messanger = new MessageBoxProvider(dialogService);
+    }
+  }
+}
